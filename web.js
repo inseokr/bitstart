9235d523436ee550ba25b9fb79a49aa3b77d6fc3
inseokr <inseo.kr@gmail.com>
2013-07-08 18:37:54 -0700

    read it from html file

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
+var fileName = "index.html"
+
+var sendResponse = function (response) {
+   fs.readFile(fileName, "utf8", function(error, data)
+   {
+      response.send(data);
+   });
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+   sendResponse(response);
 });
 
 var port = process.env.PORT || 5000;
